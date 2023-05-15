0f6701190c547526979bc130f9aa96a2bf53c03e
Carter Solberg <65569256+csolbs24@users.noreply.github.com>
2023-05-15 13:01:05 -0500

    Add config property to set default filter distance value

--- src/configuration.ts
+++ src/configuration.ts
@@ -56,8 +56,12 @@ export class Configuration {
     public delete_annotation_keybind: string = "d";
     
     public filter_low_confidence_default_value: number;
-    
+
     public filter_annotations_on_load: boolean = false;
+
+    public filter_row_distance_default_value: number;
+
+    public filter_row_distance_on_load: boolean = false;
     
     public switch_subtask_keybind: string = "z";
     
