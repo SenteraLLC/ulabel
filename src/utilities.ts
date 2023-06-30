78b7f36d37ba7f1edf5b3c621aa12b4ffdd4fbea
Carter Solberg <65569256+csolbs24@users.noreply.github.com>
2023-06-30 12:18:48 -0500

    Created `time_function`
    `time_function` is a function that wraps another function and logs how long the function took to complete. Useful for debugging.

--- src/utilities.ts
+++ src/utilities.ts
@@ -15,3 +15,20 @@ export function is_object_and_not_array(object: unknown): boolean {
         object !== null // null is an object, so make sure the object isn't null
     )
 }
+
+/**
+ * Wraps a function and logs how long the function takes to run.
+ * 
+ * @param original_function 
+ * @param function_name String to be added to the beginning of the log. Useful for stating which function was wrapped.
+ * @returns 
+ */
+export function time_function(original_function: Function, function_name: string = ""): Function {
+    function replacement_method(...args: any[]) {
+        const time_before = Date.now()
+        const result = original_function(...args)
+        console.log(`${function_name} took ${Date.now() - time_before}ms to complete.`)
+        return result
+    }
+    return replacement_method
+}
