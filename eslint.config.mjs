8c18020671866bba1ee6f765bd51c9d9fac63e2b
joshua-dean <alder314@gmail.com>
2024-09-27 13:00:27 -0500

    Add pre-commit, ESLint, prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    ignores: ["node_modules", "dist", "build", "demo.js"],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
