b4be8bc99dc6fa81e2c55cc2ec32b8599a7f84f8
joshua-dean <alder314@gmail.com>
2024-09-30 11:17:21 -0500

    Toggle on `sort-imports`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,14 +5,13 @@ import tseslint from "typescript-eslint";
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
-  },
-  {
+    ignores: ["node_modules", "dist", "build", "demo.js", "webpack.config.js"],
     languageOptions: {
       globals: globals.browser,
     },
-  },
-  {
-    ignores: ["node_modules", "dist", "build", "demo.js", "webpack.config.js"],
+    rules: {
+        "sort-imports": "error",
+    }
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
