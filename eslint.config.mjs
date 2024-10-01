5506824ca4234330afcc8d52142b6d232eccb477
joshua-dean <alder314@gmail.com>
2024-10-01 09:19:21 -0500

    Configure a few more rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,10 +27,32 @@ export default [
             "@stylistic": stylistic,
         },
         rules: {
+            "@stylistic/array-element-newline": [
+                "error",
+                {
+                    "consistent": true,
+                    "multiline": true,
+                },
+            ],
+            "@stylistic/block-spacing": ["error", "always"],
+            "@stylistic/brace-style": ["error", "1tbs"],
+            "@stylistic/comma-dangle": ["error", "always-multiline"],
+            "@stylistic/comma-spacing": [
+                "error",
+                {
+                    before: false,
+                    after: true,
+                },
+            ],
+            "@stylistic/comma-style": ["error", "last"],
+            "@stylistic/dot-location": ["error", "property"],
+            "@stylistic/eol-last": ["error", "always"],
+            "@stylistic/function-call-argument-newline": ["error", "consistent"],
+            "@stylistic/function-call-spacing": ["error", "never"],
             "@stylistic/indent": ["error", 4],
             "@stylistic/quotes": ["error", "double"],
             "@stylistic/semi": ["error", "always"],
-        }
+        },
 
     },
     eslint.configs.recommended,
