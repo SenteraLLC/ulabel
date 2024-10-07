160c5d2fcfe943f949574bf20e4f3473e1c0a437
joshua-dean <alder314@gmail.com>
2024-10-07 12:03:19 -0500

    Refactor additional listeners to handlers

--- src/listeners.ts
+++ src/listeners.ts
@@ -2,6 +2,8 @@
  * ULabel listener utilities.
  *
  * These primarily use JQuery, within the namespace "ulabel".
+ * Selectors and `.on` calls are intentionally kept in `create_ulabel_listeners`.
+ * Long handlers are broken out into separate functions.
  */
 
 import type { ULabel } from "..";
@@ -11,188 +13,183 @@ import { DELETE_CLASS_ID, DELETE_MODES, NONSPATIAL_MODES } from "./annotation";
 const ULABEL_NAMESPACE = ".ulabel";
 
 /**
- * Create the handler for keypress events.
+ * Handle keypress events.
+ *
+ * @param keypress_event Key event to handle
  * @param ulabel ULabel instance
  */
-function create_keypress_handler(
+function handle_keypress_event(
+    keypress_event: JQuery.KeyPressEvent,
     ulabel: ULabel,
 ) {
-    $(document).on(
-        "keypress" + ULABEL_NAMESPACE,
-        function (key_event) {
-            const current_subtask = ulabel.get_current_subtask();
-            switch (key_event.key) {
-                // Create a point annotation at the mouse's current location
-                case ulabel.config.create_point_annotation_keybind:
-                    // Only allow keypress to create point annotations
-                    if (current_subtask.state.annotation_mode === "point") {
-                        // Create an annotation based on the last mouse position
-                        ulabel.begin_annotation(ulabel.state["last_move"]);
-                    }
-                    break;
-                // Create a bbox annotation around the initial_crop,
-                // or the whole image if inital_crop does not exist
-                case ulabel.config.create_bbox_on_initial_crop:
-                    if (current_subtask.state.annotation_mode === "bbox") {
-                        // Default to an annotation with size of image
-                        // Create the coordinates for the bbox's spatial payload
-                        let bbox_top_left: [number, number] = [0, 0];
-                        let bbox_bottom_right: [number, number] = [
-                            ulabel.config.image_width,
-                            ulabel.config.image_height,
-                        ];
-
-                        // If an initial crop exists, use that instead
-                        // TODO (joshua-dean): can't this just be "if (ulabel.config.initial_crop)"?
-                        if (ulabel.config.initial_crop !== null && ulabel.config.initial_crop !== undefined) {
-                            // Convenience
-                            const initial_crop = ulabel.config.initial_crop;
-
-                            // Create the coordinates for the bbox's spatial payload
-                            bbox_top_left = [initial_crop.left, initial_crop.top];
-                            bbox_bottom_right = [initial_crop.left + initial_crop.width, initial_crop.top + initial_crop.height];
-                        }
-
-                        // Create the annotation
-                        ulabel.create_annotation(
-                            current_subtask.state.annotation_mode,
-                            [bbox_top_left, bbox_bottom_right],
-                        );
-                    }
-                    break;
-                // Change to brush mode (for now, polygon only)
-                case ulabel.config.toggle_brush_mode_keybind:
-                    ulabel.toggle_brush_mode(ulabel.state["last_move"]);
-                    break;
-                // Change to erase mode (will also set the is_in_brush_mode state)
-                case ulabel.config.toggle_erase_mode_keybind:
-                    ulabel.toggle_erase_mode(ulabel.state["last_move"]);
-                    break;
-                // Increase brush size by 10%
-                case ulabel.config.increase_brush_size_keybind:
-                    ulabel.change_brush_size(1.1);
-                    break;
-                // Decrease brush size by 10%
-                case ulabel.config.decrease_brush_size_keybind:
-                    ulabel.change_brush_size(1 / 1.1);
-                    break;
-                case ulabel.config.change_zoom_keybind.toLowerCase():
-                    ulabel.show_initial_crop();
-                    break;
-                case ulabel.config.change_zoom_keybind.toUpperCase():
-                    ulabel.show_whole_image();
-                    break;
-                default:
-                    // TODO (joshua-dean): break this out
-                    if (!DELETE_MODES.includes(current_subtask.state.spatial_type)) {
-                        // Check for class keybinds
-                        for (let i = 0; i < current_subtask.class_defs.length; i++) {
-                            const class_def = current_subtask.class_defs[i];
-                            if (class_def.keybind !== null && key_event.key === class_def.keybind) {
-                                const st_key = ulabel.get_current_subtask_key();
-                                const class_button = $(`#tb-id-app--${st_key} a.tbid-opt`).eq(i);
-                                if (class_button.hasClass("sel")) {
-                                    // If the class button is already selected,
-                                    // check if there is an active annotation, and if so, get it
-                                    let target_id = null;
-                                    if (current_subtask.state.active_id !== null) {
-                                        target_id = current_subtask.state.active_id;
-                                    } else if (current_subtask.state.move_candidate !== null) {
-                                        target_id = current_subtask.state.move_candidate["annid"];
-                                    }
-                                    // Update the class of the active annotation
-                                    if (target_id !== null) {
-                                        // Set the annotation's class to the selected class
-                                        ulabel.handle_id_dialog_click(
-                                            ulabel.state["last_move"],
-                                            target_id,
-                                            ulabel.get_active_class_id_idx(),
-                                        );
-                                    }
-                                } else {
-                                    // Click the class button if not already selected
-                                    class_button.trigger("click");
-                                }
-                                return;
-                            }
-                        }
-                    }
-                    break;
+    const current_subtask = ulabel.get_current_subtask();
+    switch (keypress_event.key) {
+        // Create a point annotation at the mouse's current location
+        case ulabel.config.create_point_annotation_keybind:
+            // Only allow keypress to create point annotations
+            if (current_subtask.state.annotation_mode === "point") {
+                // Create an annotation based on the last mouse position
+                ulabel.begin_annotation(ulabel.state["last_move"]);
             }
-        },
-    );
+            break;
+        // Create a bbox annotation around the initial_crop,
+        // or the whole image if inital_crop does not exist
+        case ulabel.config.create_bbox_on_initial_crop:
+            if (current_subtask.state.annotation_mode === "bbox") {
+                // Default to an annotation with size of image
+                // Create the coordinates for the bbox's spatial payload
+                let bbox_top_left: [number, number] = [0, 0];
+                let bbox_bottom_right: [number, number] = [
+                    ulabel.config.image_width,
+                    ulabel.config.image_height,
+                ];
+
+                // If an initial crop exists, use that instead
+                // TODO (joshua-dean): can't this just be "if (ulabel.config.initial_crop)"?
+                if (ulabel.config.initial_crop !== null && ulabel.config.initial_crop !== undefined) {
+                    // Convenience
+                    const initial_crop = ulabel.config.initial_crop;
+
+                    // Create the coordinates for the bbox's spatial payload
+                    bbox_top_left = [initial_crop.left, initial_crop.top];
+                    bbox_bottom_right = [initial_crop.left + initial_crop.width, initial_crop.top + initial_crop.height];
+                }
+
+                // Create the annotation
+                ulabel.create_annotation(
+                    current_subtask.state.annotation_mode,
+                    [bbox_top_left, bbox_bottom_right],
+                );
+            }
+            break;
+        // Change to brush mode (for now, polygon only)
+        case ulabel.config.toggle_brush_mode_keybind:
+            ulabel.toggle_brush_mode(ulabel.state["last_move"]);
+            break;
+        // Change to erase mode (will also set the is_in_brush_mode state)
+        case ulabel.config.toggle_erase_mode_keybind:
+            ulabel.toggle_erase_mode(ulabel.state["last_move"]);
+            break;
+        // Increase brush size by 10%
+        case ulabel.config.increase_brush_size_keybind:
+            ulabel.change_brush_size(1.1);
+            break;
+        // Decrease brush size by 10%
+        case ulabel.config.decrease_brush_size_keybind:
+            ulabel.change_brush_size(1 / 1.1);
+            break;
+        case ulabel.config.change_zoom_keybind.toLowerCase():
+            ulabel.show_initial_crop();
+            break;
+        case ulabel.config.change_zoom_keybind.toUpperCase():
+            ulabel.show_whole_image();
+            break;
+        default:
+            // TODO (joshua-dean): break this out
+            if (!DELETE_MODES.includes(current_subtask.state.spatial_type)) {
+                // Check for class keybinds
+                for (let i = 0; i < current_subtask.class_defs.length; i++) {
+                    const class_def = current_subtask.class_defs[i];
+                    if (class_def.keybind !== null && keypress_event.key === class_def.keybind) {
+                        const st_key = ulabel.get_current_subtask_key();
+                        const class_button = $(`#tb-id-app--${st_key} a.tbid-opt`).eq(i);
+                        if (class_button.hasClass("sel")) {
+                            // If the class button is already selected,
+                            // check if there is an active annotation, and if so, get it
+                            let target_id = null;
+                            if (current_subtask.state.active_id !== null) {
+                                target_id = current_subtask.state.active_id;
+                            } else if (current_subtask.state.move_candidate !== null) {
+                                target_id = current_subtask.state.move_candidate["annid"];
+                            }
+                            // Update the class of the active annotation
+                            if (target_id !== null) {
+                                // Set the annotation's class to the selected class
+                                ulabel.handle_id_dialog_click(
+                                    ulabel.state["last_move"],
+                                    target_id,
+                                    ulabel.get_active_class_id_idx(),
+                                );
+                            }
+                        } else {
+                            // Click the class button if not already selected
+                            class_button.trigger("click");
+                        }
+                        return;
+                    }
+                }
+            }
+            break;
+    }
 }
 
 /**
- * Create a listener for the soft ID toolbox button.
+ * Handle a click on a soft ID toolbox button.
+ *
+ * @param click_event Click event
  * @param ulabel ULabel instance
  */
-function create_soft_id_toolbox_button_listener(
+function handle_soft_id_toolbox_button_click(
+    click_event: JQuery.ClickEvent,
     ulabel: ULabel,
 ) {
-    $(document).on(
-        "click" + ULABEL_NAMESPACE,
-        `#${ulabel.config["toolbox_id"]} a.tbid-opt`,
-        function (click_event) {
-            const tgt_jq = $(click_event.currentTarget);
-            const pfx = "div#tb-id-app--" + ulabel.get_current_subtask_key();
-            const current_subtask = ulabel.get_current_subtask();
-            if (tgt_jq.attr("href") === "#") {
-                const current_id_button = $(pfx + " a.tbid-opt.sel");
-                current_id_button.attr("href", "#");
-                current_id_button.removeClass("sel");
-                const old_id = parseInt(current_id_button.attr("id").split("_").at(-1));
-                tgt_jq.addClass("sel");
-                tgt_jq.removeAttr("href");
-                const idarr = tgt_jq.attr("id").split("_");
-                const rawid = parseInt(idarr[idarr.length - 1]);
-                ulabel.set_id_dialog_payload_nopin(
-                    current_subtask["class_ids"].indexOf(rawid),
-                    1.0,
-                );
-                ulabel.update_id_dialog_display();
+    const tgt_jq = $(click_event.currentTarget);
+    const pfx = "div#tb-id-app--" + ulabel.get_current_subtask_key();
+    const current_subtask = ulabel.get_current_subtask();
+    if (tgt_jq.attr("href") === "#") {
+        const current_id_button = $(pfx + " a.tbid-opt.sel");
+        current_id_button.attr("href", "#");
+        current_id_button.removeClass("sel");
+        const old_id = parseInt(current_id_button.attr("id").split("_").at(-1));
+        tgt_jq.addClass("sel");
+        tgt_jq.removeAttr("href");
+        const idarr = tgt_jq.attr("id").split("_");
+        const rawid = parseInt(idarr[idarr.length - 1]);
+        ulabel.set_id_dialog_payload_nopin(
+            current_subtask["class_ids"].indexOf(rawid),
+            1.0,
+        );
+        ulabel.update_id_dialog_display();
 
-                // Update the class of the active annotation,
-                // except when toggling on the delete class
-                if (rawid !== DELETE_CLASS_ID) {
-                    // Get the active annotation, if any
-                    let target_id = null;
-                    if (current_subtask.state.active_id !== null) {
-                        target_id = current_subtask.state.active_id;
-                    } else if (current_subtask.state.move_candidate !== null) {
-                        target_id = current_subtask.state.move_candidate["annid"];
-                    }
-
-                    // Update the class of the active annotation
-                    if (target_id !== null) {
-                        // Set the annotation's class to the selected class
-                        ulabel.handle_id_dialog_click(
-                            ulabel.state["last_move"],
-                            target_id,
-                            ulabel.get_active_class_id_idx(),
-                        );
-                    } else {
-                        // If there is not active annotation,
-                        // still update the brush circle if in brush mode
-                        ulabel.recolor_brush_circle();
-                    }
-                }
-
-                /*
-                If toggling off a delete class while still in delete mode,
-                re-toggle the delete class.
-                This occurs when using a keybind to change a hovered annotation's
-                class while in delete mode.
-                */
-                if (
-                    old_id === DELETE_CLASS_ID &&
-                    DELETE_MODES.includes(current_subtask.state.annotation_mode)
-                ) {
-                    $("#toolbox_sel_" + DELETE_CLASS_ID).trigger("click");
-                }
+        // Update the class of the active annotation,
+        // except when toggling on the delete class
+        if (rawid !== DELETE_CLASS_ID) {
+            // Get the active annotation, if any
+            let target_id = null;
+            if (current_subtask.state.active_id !== null) {
+                target_id = current_subtask.state.active_id;
+            } else if (current_subtask.state.move_candidate !== null) {
+                target_id = current_subtask.state.move_candidate["annid"];
             }
-        },
-    );
+
+            // Update the class of the active annotation
+            if (target_id !== null) {
+                // Set the annotation's class to the selected class
+                ulabel.handle_id_dialog_click(
+                    ulabel.state["last_move"],
+                    target_id,
+                    ulabel.get_active_class_id_idx(),
+                );
+            } else {
+                // If there is not active annotation,
+                // still update the brush circle if in brush mode
+                ulabel.recolor_brush_circle();
+            }
+        }
+
+        /*
+        If toggling off a delete class while still in delete mode,
+        re-toggle the delete class.
+        This occurs when using a keybind to change a hovered annotation's
+        class while in delete mode.
+        */
+        if (
+            old_id === DELETE_CLASS_ID &&
+            DELETE_MODES.includes(current_subtask.state.annotation_mode)
+        ) {
+            $("#toolbox_sel_" + DELETE_CLASS_ID).trigger("click");
+        }
+    }
 }
 
 /**
@@ -245,7 +242,7 @@ function handle_keydown_event(
 
 /**
  * Create listeners for a ULabel instance.
- * Consider breaking out anything longer than 10 lines.
+ * Consider breaking out the handler if longer than 10 lines.
  *
  * @param ulabel ULabel instance
  */
@@ -305,7 +302,12 @@ export function create_ulabel_listeners(
 
     // ================= Uncategorized =================
 
-    create_keypress_handler(ulabel);
+    $(document).on(
+        "keypress" + ULABEL_NAMESPACE,
+        function (keypress_event: JQuery.KeyPressEvent) {
+            handle_keypress_event(keypress_event, ulabel);
+        },
+    );
 
     // This listener does not use jquery because it requires being able to prevent default
     // There are maybe some hacky ways to do this with jquery
@@ -344,7 +346,14 @@ export function create_ulabel_listeners(
     // Store a reference
     ulabel.resize_observers.push(tb_overflow_resize_observer);
 
-    create_soft_id_toolbox_button_listener(ulabel);
+    // create_soft_id_toolbox_button_listener(ulabel);
+    $(document).on(
+        "click" + ULABEL_NAMESPACE,
+        `#${ulabel.config["toolbox_id"]} a.tbid-opt`,
+        function (click_event: JQuery.ClickEvent) {
+            handle_soft_id_toolbox_button_click(click_event, ulabel);
+        },
+    );
 
     $(document).on(
         "click" + ULABEL_NAMESPACE,
