91a256d389e00b07aebadd1ac315bd4be657cb58
joshua-dean <alder314@gmail.com>
2025-07-22 10:46:30 -0500

    Fix "Annotation ID" text color in night mode

--- src/blobs.js
+++ src/blobs.js
@@ -1997,6 +1997,11 @@ div#${prntid} span.tb-cls-nam {
    display: inline-block;
    vertical-align: middle;
 }
+div#${prntid}.ulabel-night span.tb-cls-nam {
+   display: inline-block;
+   vertical-align: middle;
+   color: white;
+}
 div#${prntid} a.tbid-opt:hover {
    background-color: rgba(255, 181, 44, 0.397);
 }
